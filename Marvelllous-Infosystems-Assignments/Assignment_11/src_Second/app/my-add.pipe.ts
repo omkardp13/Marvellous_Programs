773d3547505f1f0af74e3dd2d7bb5e510628bff1
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-29 17:37:03 +0530

    Add files via upload

---
+++ Marvelllous-Infosystems-Assignments/Assignment_11/src_Second/app/my-add.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'myAdd'
+})
+export class MyAddPipe implements PipeTransform {
+
+  transform(value: number, ...args: number[]): number
+  {
+    return value + args[0];
+  }
+
+}
